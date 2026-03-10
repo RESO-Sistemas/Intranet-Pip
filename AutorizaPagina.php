5e04909217c22e43b92cd63a47b86a0e9e892e5f
carlitospro2003 <carloseduardorsaucedo2003@gmail.com>
2026-03-10 16:01:00 -0600

    basename

--- AutorizaPagina.php
+++ AutorizaPagina.php
@@ -8,7 +8,7 @@ SessionManager::requireLogin();
 
 // Validación de permisos por puesto
 $Empleados = new Empleados();
-$VerificaPermisoPagina = $Empleados->autorizaPermisoPagina($_SERVER['REQUEST_URI']);
+$VerificaPermisoPagina = $Empleados->autorizaPermisoPagina(basename($_SERVER['REQUEST_URI']));
 
 if ($VerificaPermisoPagina == "0") {
   echo '<meta http-equiv="refresh" content="0;url=index.php">';
